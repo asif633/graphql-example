1b3d0783909818e1c0b1c5060e0563097e9c2c7c
Asif Iqubal <asif633@gmail.com>
2017-07-30 13:40:01 +0530

    Added mutation

--- schema/schema.js
+++ schema/schema.js
@@ -18,9 +18,9 @@ const CompanyType = new GraphQLObjectType({
         desc: { type: GraphQLString },
         users: {
             type: new GraphQLList(UserType),
-            resolve(parentValue, args){
+            resolve(parentValue, args) {
                 return axios.get(`http://localhost:3000/companies/${parentValue.id}/users`)
-                .then(res => res.data);
+                    .then(res => res.data);
             }
         }
     })
@@ -64,6 +64,48 @@ const RootQuery = new GraphQLObjectType({
     }
 });
 
+const mutation = new GraphQLObjectType({
+    name: 'Mutation',
+    fields: {
+        addUser: {
+            type: UserType, // return type
+            args: {
+                firstName: { type: new GraphQLNonNull(GraphQLString) },
+                age: { type: new GraphQLNonNull(GraphQLInt) },
+                companyId: { type: GraphQLString }
+            },
+            resolve(parentValue, { firstName, age }) {
+                return axios.post(`http://localhost:3000/users`, { firstName, age })
+                    .then(res => res.data);
+            }
+        },
+        deleteUser: {
+            type: UserType, // return type
+            args: {
+                id: { type: new GraphQLNonNull(GraphQLString) }
+            },
+            resolve(parentValue, { id }) {
+                return axios.delete(`http://localhost:3000/users/${id}`)
+                    .then(res => res.data);
+            }
+        },
+        editUser: {
+            type: UserType, // return type
+            args: {
+                id: { type: new GraphQLNonNull(GraphQLString) },
+                firstName: { type: GraphQLString },
+                age: { type: GraphQLInt },
+                companyId: { type: GraphQLString }
+            },
+            resolve(parentValue, args) {
+                return axios.patch(`http://localhost:3000/users/${args.id}`, args)
+                    .then(res => res.data);
+            }
+        }
+    }
+})
+
 module.exports = new GraphQLSchema({
-    query: RootQuery
+    query: RootQuery,
+    mutation
 })
